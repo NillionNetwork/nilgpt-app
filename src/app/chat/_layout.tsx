8896a3b34de8603ad86dac67c0244b117d50be9e
Dev <devanandgowda8@gmail.com>
2025-12-12 01:41:28 +0530

    fix: new chat screen without id after entering pin

--- src/app/chat/_layout.tsx
+++ src/app/chat/_layout.tsx
@@ -1,13 +1,18 @@
 import Sidebar from '@/components/Sidebar';
 import Drawer from 'expo-router/drawer';
+import { v4 as uuidv4 } from 'uuid';
 
 const ChatLayout: React.FC = () => {
   return (
     <Drawer
       backBehavior="none"
       drawerContent={(props) => <Sidebar {...props} />}
-      screenOptions={{ headerShown: false, drawerHideStatusBarOnOpen: true }}
-    />
+      screenOptions={{ headerShown: false, drawerHideStatusBarOnOpen: true }}>
+      <Drawer.Screen
+        name="[id]"
+        initialParams={{ id: uuidv4(), newChat: 'true' }}
+      />
+    </Drawer>
   );
 };
 
